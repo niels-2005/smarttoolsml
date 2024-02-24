529e68ddd47aea10325d8cd78c954fd64fe0f715
niels-2005 <niels.scholz01@web.de>
2024-02-24 11:40:13 +0100

    optimized sequence classification pipelines

--- smarttoolsml/tf_templates/nlp/sequence_classification.py
+++ smarttoolsml/tf_templates/nlp/sequence_classification.py
@@ -1,8 +1,11 @@
 import tensorflow as tf
 import transformers
+import torch
+
+Tensor = tf.Tensor
 
 
-def build_nlp_classifier(
+def build_keras_nlp_classifier(
     classifier,
     preset_name: str,
     num_classes: int,
@@ -70,74 +73,109 @@ def build_nlp_classifier(
     return classifier
 
 
-def sequence_classification_pipeline(
+def tokenize_features_and_prep_labels(
     texts: list[str],
     labels: list[int],
     tokenizer: transformers.PreTrainedTokenizer,
+    preset_name: str,
+    padding: bool = True,
+    truncation: bool = True,
+    return_tensors: str = "tf",
+) -> tuple[dict, Tensor]:
+    """
+    Tokenizes input texts and prepares labels for training using using a pre-trained tokenizer from the Hugging Face's Transformers library.
+    This function is designed to preprocess data for NLP models, making it ready for model training or evaluation.
+
+    Args:
+        texts (list[str]): List of text strings to be tokenized, representing the input data for the NLP model.
+        labels (list[int]): List of integer labels associated with each input text, used for model training.
+        tokenizer (transformers.PreTrainedTokenizer): An instance of a tokenizer, pre-trained and provided by
+            Hugging Face's Transformers library, used to tokenize the input texts.
+        preset_name (str): Name of the pre-trained tokenizer to use for tokenizing input texts. This parameter
+            is used to ensure that the tokenizer is properly configured if additional setup is required.
+        padding (bool, optional): Specifies whether to pad the tokenized input sequences to the longest sequence
+            in the batch. Defaults to True.
+        truncation (bool, optional): Specifies whether to truncate the tokenized input sequences to the model's
+            maximum input length. Defaults to True.
+        return_tensors (str, optional): Specifies the type of tensors to return ('tf' for TensorFlow tensors,
+            'pt' for PyTorch tensors). Defaults to 'tf'.
+
+    Returns:
+        tuple: A tuple containing two elements:
+            - encoded_input (dict): A dictionary containing the tokenized input data, ready to be fed into an NLP model.
+            - labels (tf.Tensor or torch.Tensor): A tensor of the labels, ready to be used in model training or evaluation.
+
+    Example usage:
+        from transformers import BertTokenizer
+        texts = ["Hello, world!", "Machine learning is fascinating."]
+        labels = [0, 1]
+        tokenizer = BertTokenizer
+
+        encoded_input, labels_tensor = tokenize_features_and_prep_labels(
+            texts=texts,
+            labels=labels,
+            tokenizer=tokenizer,
+            preset_name='bert-base-uncased',
+            padding=True,
+            truncation=True,
+            return_tensors="tf"
+        )
+
+        # Now, `encoded_input` can be used as input to a model, and `labels_tensor` as the target labels for training or evaluation.
+    """
+    tokenizer = tokenizer.from_pretrained(preset_name)
+    encoded_input = tokenizer(
+        texts, padding=padding, truncation=truncation, return_tensors=return_tensors
+    )
+    labels_tensor = (
+        tf.constant(labels) if return_tensors == "tf" else torch.tensor(labels)
+    )
+
+    return encoded_input, labels_tensor
+
+
+def build_sequence_classification_model(
     preset_name: str,
     model_task: transformers.PreTrainedModel,
     num_classes: int,
     optimizer,
     loss,
     trainable: bool = True,
-    padding: bool = True,
-    truncation: bool = True,
-    return_tensors: str = "tf",
-):
+) -> transformers.PreTrainedModel:
     """
-    Prepares an NLP model for training by tokenizing input texts, initializing and
-    configuring the model, and then compiling it.
+    Initializes and compiles a sequence classification model using a pre-trained model from the Hugging Face's Transformers library.
+
+    This function allows for the customization of the sequence classification model by specifying the number of classes,
+    optimizer, loss function, and whether the model's parameters should be trainable.
 
     Args:
-        texts (List[str]): List of text strings representing the input data.
-        labels (List[int]): List of integer labels associated with the input data.
-        tokenizer (transformers.PreTrainedTokenizer): Tokenizer class from Hugging Face's Transformers.
-        preset_name (str): Name of the pre-trained model to be used.
-        model_task (transformers.TFPreTrainedModel): Model class for the specific task.
-        num_classes (int): Number of classes for the classification task.
-        optimizer (str or tf.keras.optimizers.Optimizer): Optimizer for training the model.
-        loss (str or tf.keras.losses.Loss): Loss function for training the model.
-        trainable (bool, optional): Specifies whether the model's backbone should be trainable. Defaults to True.
-        padding (bool): Whether to pad sequences to the same length.
-        truncation (bool): Whether to truncate long sequences.
-        return_tensors (str): The format of the returned tensors, typically 'tf'.
+        preset_name (str): The name of the pre-trained model to be used as the base for the sequence classification task.
+        model_task (transformers.PreTrainedModel): The class from Hugging Face's Transformers library representing the model.
+        num_classes (int): The number of classes for the classification task.
+        optimizer: The optimizer to use for training the model.
+        loss: The loss function to use for training the model.
+        trainable (bool, optional): Whether the model's weights should be trainable. Defaults to True.
 
     Returns:
-        model (TFPreTrainedModel): The prepared and compiled model.
-        encoded_input (Dict[tf.Tensor]): Tokenized input data as tensors.
-        labels (tf.Tensor): Labels as a tensor.
+        transformers.PreTrainedModel: The compiled sequence classification model ready for training.
 
     Example usage:
-        from transformers import BertTokenizerFast, TFBertForSequenceClassification
+        from transformers import TFBertForSequenceClassification
         from tensorflow.keras.optimizers import Adam
+        from tensorflow.keras.losses import SparseCategoricalCrossentropy
 
-        texts = ["Hello, world!", "Machine learning is amazing."]
-        labels = [0, 1]
-        tokenizer = BertTokenizerFast
-        preset_name = 'bert-base-uncased'
-        model_task = TFBertForSequenceClassification
-
-        model, encoded_input, labels = sequence_classification_pipeline(
-            texts=texts,
-            labels=labels,
-            tokenizer=tokenizer,
-            preset_name=preset_name,
-            model_task=model_task,
+        model = build_sequence_classification_model(
+            preset_name='bert-base-uncased',
+            model_task=TFBertForSequenceClassification,
             num_classes=2,
             optimizer=Adam(learning_rate=1e-4),
-            loss='sparse_categorical_crossentropy',
+            loss=SparseCategoricalCrossentropy(from_logits=True),
             trainable=True
         )
 
-        model.fit(encoded_input, labels, epochs=2, batch_size=2)
+        # Model is now compiled and ready for training with your dataset.
     """
-    tokenizer = tokenizer.from_pretrained(preset_name)
     model = model_task.from_pretrained(preset_name, num_labels=num_classes)
-    encoded_input = tokenizer(
-        texts, padding=padding, truncation=truncation, return_tensors=return_tensors
-    )
     model.trainable = trainable
-    labels = tf.constant(labels)
     model.compile(optimizer=optimizer, loss=loss, metrics=["accuracy"])
-
-    return model, encoded_input, labels
+    return model
