82b47e40c3adf6eb238fb9a95ca944af5d9860fb
niels-2005 <niels.scholz01@web.de>
2024-02-27 19:17:47 +0100

    added preprocessing gemma func

--- smarttoolsml/torch_templates/nlp/gemma/helper_gemma.py
+++ smarttoolsml/torch_templates/nlp/gemma/helper_gemma.py
@@ -1,6 +1,24 @@
 import keras
 import keras_nlp
 
+
+def preprocess_df_to_gemma(file, instruction, output):
+    """
+    Preprocesses a DataFrame for use with Gemma by formatting instructions and responses.
+
+    Args:
+        file (pd.DataFrame): The DataFrame containing the data to be processed.
+        instruction (str): The name of the column in 'file' that contains the instructions.
+        output (str): The name of the column in 'file' that contains the expected outputs.
+
+    Returns:
+        list: A list of formatted strings, each combining an instruction and its associated response from the DataFrame.
+    """
+    data = file.apply(lambda row: f'Instruction:\n{row[instruction]}\n\nResponse:{row[output]}', axis=1).values.tolist()
+    return data
+    
+
+
 def generate_response(instruction: str, gemma_model: keras.Model, max_length: int = 512):
     """
     Generates a response using a Gemma model based on the provided instruction.
