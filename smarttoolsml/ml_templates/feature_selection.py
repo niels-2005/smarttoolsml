90ce25d4e6f7771839a734b401b5d1060941bff0
niels-2005 <niels.scholz01@web.de>
2024-04-29 07:54:15 +0200

    added feature selection template

--- smarttoolsml/ml_templates/feature_selection.py
+++ smarttoolsml/ml_templates/feature_selection.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.feature_selection import RFECV, SelectKBest, chi2
+from sklearn.preprocessing import MinMaxScaler
 
 
 def get_k_best_features(
@@ -16,6 +17,11 @@ def get_k_best_features(
 
     Returns:
         _type_: _description_
+
+    Example usage:
+        X_norm = MinMaxScaler().fit_transform(X)
+        get_k_best_features(k=5, X=X_norm, y=y_train)
+
     """
     selected_features = SelectKBest(chi2, k=k).fit(X, y)
     print("Score list:", selected_features.scores_)
@@ -48,7 +54,8 @@ def rfecv_feature_selector(
 
     Example usage:
         rfc = RandomForestClassifier()
-        rfecv = rfecv_feature_selector(model=rfc, X, y, cv=5, plot_curve=True)
+        cv = StratifiedKFold(n_splits=5)
+        rfecv = rfecv_feature_selector(model=rfc, X, y, cv=cv, plot_curve=True)
     """
     rfecv = RFECV(estimator=model, step=step, cv=cv, scoring=scoring)
     rfecv.fit(X, y)
@@ -68,3 +75,34 @@ def rfecv_feature_selector(
         plt.show()
 
     return rfecv
+
+
+def cor_selector(X: np.ndarray, y: np.ndarray, num_feats: int):
+    """_summary_
+
+    Args:
+        X (np.ndarray): _description_
+        y (np.ndarray): _description_
+        num_feats (int): _description_
+
+    Returns:
+        _type_: _description_
+
+    Example usage:
+        cor_support, cor_feature = cor_selector(X, y,num_feats)
+        (str(len(cor_feature)), 'selected features')
+
+    """
+    cor_list = []
+    feature_name = X.columns.tolist()
+    # calculate the correlation with y for each feature
+    for i in X.columns.tolist():
+        cor = np.corrcoef(X[i], y)[0, 1]
+        cor_list.append(cor)
+    # replace NaN with 0
+    cor_list = [0 if np.isnan(i) else i for i in cor_list]
+    # feature name
+    cor_feature = X.iloc[:, np.argsort(np.abs(cor_list))[-num_feats:]].columns.tolist()
+    # feature selection? 0 for not select, 1 for select
+    cor_support = [True if i in cor_feature else False for i in feature_name]
+    return cor_support, cor_feature
