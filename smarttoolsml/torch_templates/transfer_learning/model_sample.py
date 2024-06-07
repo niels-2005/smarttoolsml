947d2aeda87dbe19d0bb4acdef5bb17b404400eb
niels-2005 <niels.scholz01@web.de>
2024-06-07 13:53:17 +0200

    added pytorch templates

--- smarttoolsml/torch_templates/transfer_learning/model_sample.py
+++ smarttoolsml/torch_templates/transfer_learning/model_sample.py
@@ -7,14 +7,12 @@ def get_model(
     classes: list,
     freeze_layers: bool = True,
     seed: int = 42,
-    device: torch.device = "cuda" if torch.cuda.is_available() else "cpu",
 ):
-
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
 
     weights = torchvision.models.EfficientNet_B0_Weights.DEFAULT
-    model = torchvision.models.efficientnet_b0(weights=weights).to(device)
+    model = torchvision.models.efficientnet_b0(weights=weights)
 
     if freeze_layers:
         for param in model.features.parameters():
@@ -24,8 +22,8 @@ def get_model(
 
     # in_features = 1280 (because efficientnetb0)
     model.classifer = nn.Sequential(
-        nn.Dropout(0.5),
-        nn.Linear(in_features=1280, out_features=output_shape, bias=True).to(device),
+        nn.Dropout(0.5, inplace=True),
+        nn.Linear(in_features=1280, out_features=output_shape, bias=True),
     )
 
     return model
