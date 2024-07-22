08c27ecad49b032b3090f52ea1c7bd0c0b00701f
niels-2005 <niels.scholz01@web.de>
2024-07-22 19:33:33 +0200

    added grouped by template

---
+++ smarttoolsml/ml_templates/group_by.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+
+
+def group_by(df: pd.DataFrame):
+    """_summary_
+
+    grouped by Location Temperature
+
+    Args:
+        df (pd.DataFrame): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    grouped = df.groupby("Location")["Temperature"].agg(["mean", "min", "max", "count"])
+    return grouped
