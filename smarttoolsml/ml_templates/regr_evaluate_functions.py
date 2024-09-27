b8cf21d29ec5c8ad9f3f5e8e8b554211dce0e861
Niels Scholz <130656751+niels-2005@users.noreply.github.com>
2024-09-27 20:10:18 +0200

    Update regr_evaluate_functions.py

--- smarttoolsml/ml_templates/regr_evaluate_functions.py
+++ smarttoolsml/ml_templates/regr_evaluate_functions.py
@@ -1,1 +1,21 @@
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import numpy as np
+import pandas as pd
 
+def calculate_metrics(y_true: np.ndarray, y_pred: np.ndarray):
+    mae = mean_absolute_error(y_true, y_pred)
+    mse = mean_squared_error(y_true, y_pred)
+    rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+    rmsle = np.log(np.sqrt(mean_squared_error(y_true, y_pred)))
+    r2 = r2_score(y_true, y_pred)
+
+    df_dict = {
+        "mae": mae,
+        "mse": mse,
+        "rmse": rmse,
+        "rmsle": rmsle,
+        "r2": r2
+    }
+
+    df = pd.DataFrame(df_dict)
+    return df
