870d08de872ec9392de9c1cef2f4a2b0e0d181c2
Niels Scholz <130656751+niels-2005@users.noreply.github.com>
2024-11-09 14:33:54 +0100

    Update unittest_template_small.py

--- smarttoolsml/test_templates/unittest_template_small.py
+++ smarttoolsml/test_templates/unittest_template_small.py
@@ -1,19 +1,19 @@
 def check_permutation_by_sort(s1: str, s2: str) -> bool:
     if len(s1) != len(s2):
-        return False 
-    
+        return False
+
     s1, s2 = sorted(s1), sorted(s2)
 
     for i in range(len(s1)):
         if s1[i] != s2[i]:
-            return False 
+            return False
     return True
 
 
 def check_permutation_by_count(s1: str, s2: str) -> bool:
     if len(s1) != len(s2):
-        return False 
-    
+        return False
+
     # assuming ascii
     counter = [0] * 128
 
@@ -22,29 +22,28 @@ def check_permutation_by_count(s1: str, s2: str) -> bool:
 
     for char in s2:
         if counter[ord(char)] == 0:
-            return False 
+            return False
         counter[ord(char)] -= 1
     return True
 
+
 import unittest
+
 
 class Test(unittest.TestCase):
     test_cases = [
         ("dog", "god", True),
         ("dog ", "god ", True),
         ("dog", "cat", False),
-        ("DOG", "god", False)
+        ("DOG", "god", False),
     ]
 
-    test_functions = [
-        check_permutation_by_count, 
-        check_permutation_by_sort
-    ]
+    test_functions = [check_permutation_by_count, check_permutation_by_sort]
 
     def test_permutation(self):
         for check_permutation in self.test_functions:
             for s1, s2, expected in self.test_cases:
-                assert check_permutation(s1, s2) == expected 
+                assert check_permutation(s1, s2) == expected
 
 
 if __name__ == "__main__":
