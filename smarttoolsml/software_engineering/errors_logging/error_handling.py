624157d7b6720e61dd3caf4c6b72fe430b386d72
niels-2005 <niels.scholz01@web.de>
2025-02-23 15:42:41 +0100

    added error handling and logging

---
+++ smarttoolsml/software_engineering/errors_logging/error_handling.py
@@ -0,0 +1,29 @@
+from scipy.stats import linregress
+import pandas as pd
+
+
+def fit_trendline(year_timesteps, data):
+    try:
+        # works if all is fine
+        result = linregress(year_timesteps, data)
+    except TypeError:
+        # if "TypeError" occurs, handling the error and return default values (other functions might crash)
+        print(f"Both lists must contain only float or integers,
+              got {data.dtype} and {year_timesteps.dtype} instead.")
+        return 0.0, 0.0 
+    else:
+        # if no error occurs
+        slope = round(result.slope, 3)
+        r_squared = round(result.rvalue**2, 3)
+        return slope, r_squared
+    finally:
+        # after else statement delete for memory
+        del slope, r_squared
+
+
+def load_data(path):
+    try:
+        df = pd.read_csv(path)
+        return df 
+    except Exception as e:
+        raise e
--- smarttoolsml/try_except.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def load_data(url: str) -> pd.DataFrame:
-    try:
-        df = pd.read_csv(url, sep=";")
-        return df
-    except Exception as e:
-        raise e
