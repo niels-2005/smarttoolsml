c8f03defc64f4f67d75d398caeeea2c4319a9728
Niels <niels.scholz01@web.de>
2025-07-16 23:26:15 +0200

    added model registry

---
+++ smarttoolsml/mlflow/deploy/model_registry/overview.py
@@ -0,0 +1,90 @@
+import mlflow
+
+# Option 1: specify `registered_model_name` parameter when logging a model
+mlflow.log_model(..., registered_model_name="<YOUR_MODEL_NAME>")
+
+# Option 2: register a logged model
+mlflow.register_model(model_uri="<YOUR_MODEL_URI>", name="<YOUR_MODEL_NAME>")
+
+# LOAD MODEL
+mlflow.load_model("models:/<YOUR_MODEL_NAME>/<YOUR_MODEL_VERSION>")
+
+
+# STEP 1: REGISTER A MODEL
+
+from sklearn.datasets import make_regression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+import mlflow
+import mlflow.sklearn
+
+with mlflow.start_run() as run:
+    X, y = make_regression(n_features=4, n_informative=2, random_state=0, shuffle=False)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    params = {"max_depth": 2, "random_state": 42}
+    model = RandomForestRegressor(**params)
+    model.fit(X_train, y_train)
+
+    # Log parameters and metrics using the MLflow APIs
+    mlflow.log_params(params)
+
+    y_pred = model.predict(X_test)
+    mlflow.log_metrics({"mse": mean_squared_error(y_test, y_pred)})
+
+    # Log the sklearn model and register as version 1
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        name="sklearn-model",
+        input_example=X_train,
+        registered_model_name="sk-learn-random-forest-reg-model",
+    )
+
+
+# LOAD A REGISTERED MODEL
+
+import mlflow.sklearn
+from sklearn.datasets import make_regression
+
+model_name = "sk-learn-random-forest-reg-model"
+model_version = "latest"
+
+# Load the model from the Model Registry
+model_uri = f"models:/{model_name}/{model_version}"
+model = mlflow.sklearn.load_model(model_uri)
+
+# Generate a new dataset for prediction and predict
+X_new, _ = make_regression(n_features=4, n_informative=2, random_state=0, shuffle=False)
+y_pred_new = model.predict(X_new)
+
+print(y_pred_new)
+
+
+# LOAD MODEL VIA VERSION ALIAS
+
+import mlflow.sklearn
+from mlflow import MlflowClient
+
+client = MlflowClient()
+
+# Set model version alias
+model_name = "sk-learn-random-forest-reg-model"
+model_version_alias = "the_best_model_ever"
+client.set_registered_model_alias(
+    model_name, model_version_alias, "1"
+)  # Duplicate of step in UI
+
+# Get information about the model
+model_info = client.get_model_version_by_alias(model_name, model_version_alias)
+model_tags = model_info.tags
+print(model_tags)
+
+# Get the model version using a model URI
+model_uri = f"models:/{model_name}@{model_version_alias}"
+model = mlflow.sklearn.load_model(model_uri)
+
+print(model)
